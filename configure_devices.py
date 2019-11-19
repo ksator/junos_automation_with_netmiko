1b577ed5b1719a8055ae4b3491ff6f71f43c6b6b
ksator <khelil.sator.git@gmail.com>
2019-11-19 11:32:10 -0800

    add junos states  audit

--- configure_devices.py
+++ configure_devices.py
@@ -1,7 +1,9 @@
-
 from jinja2 import Template
 from yaml import load, FullLoader
 from netmiko import ConnectHandler
+import json
+import time
+from pprint import pprint as pp
 
 # template to configure junos
 f=open('junos.j2')
@@ -13,8 +15,9 @@ f=open('vars.yml', 'r')
 vars = load(f.read(), Loader=FullLoader)
 f.close()
 
+print("generating and loading configuration")
 for item in vars:
-   host_vars = vars[item]   
+   host_vars = vars[item]
    device = host_vars['netmiko']
    dev = ConnectHandler(**device)
    f=open(host_vars['netmiko']['host'] + '.txt','w')
@@ -23,4 +26,18 @@ for item in vars:
    load = dev.send_config_from_file(config_file=host_vars['netmiko']['host'] + '.txt')
    commit = dev.commit(confirm=False, check=False, comment='commited from netmiko')
    dev.disconnect()
+
+print ("waiting 15 seconds before validating BGP sessions state")
+time.sleep(15)
+for item in vars:
+   host_vars = vars[item]
+   device = host_vars['netmiko']
+   dev = ConnectHandler(**device)
+   command = "show bgp neighbor"
+   command_output = dev.send_command(command + "| display json")
+   data_json = json.loads(command_output)
+   print ("Device " + host_vars['netmiko']['host'])
+   for neighbor in data_json["bgp-information"][0]["bgp-peer"]: 
+     print ("session with " + neighbor["peer-address"][0]['data'] + " is "+ neighbor["peer-state"][0]['data'])  
+
 
